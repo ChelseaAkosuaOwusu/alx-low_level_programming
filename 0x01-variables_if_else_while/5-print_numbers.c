fa12515a71312af30781b18914f6d3a3ecdf8aa2
ChelseaAkosuaOwusu <chelseaowusuakosua@gmail.com>
2023-02-17 15:00:32 -0800

    add all files

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int numbers[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9}, i;
+int size = sizeof(numbers) / sizeof(*numbers);
+
+	for (i = 0; i < size; i++)
+	{
+		printf("%d", numbers[i]);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
+
