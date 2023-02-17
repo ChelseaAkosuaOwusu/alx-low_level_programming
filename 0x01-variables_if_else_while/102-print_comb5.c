e02d18e4b784a819d1d18f543c2628424c5ae05a
ChelseaAkosuaOwusu <chelseaowusuakosua@gmail.com>
2023-02-17 15:11:54 -0800

    correction

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -3,32 +3,33 @@
 /**
  * main - Entry Point
  *
- * Description: prints all possible different combinations of three digits
+ * Description: prints all possible combinations of two two-digit numbers
  *
  * Return: Always 0(Success)
  */
 
 int main(void)
 {
-	int i, j, k;
+	int i, j;
 
-	for (i = 0; i < 8; i++)
+	for (i = 0; i < 99; i++)
 	{
-		for (j = i + 1; j < 9; j++)
+		for (j = i + 1; j <= 99; j++)
 		{
-			for (k = j + 1; k < 10; k++)
-			{
-				putchar('0' + i % 10);
-				putchar('0' + j % 10);
-				putchar('0' + k % 10);
-				if (i == 7 && j == 8 && k == 9)
-					break;
-				putchar(',');
-				putchar(' ');
-			}
+			putchar('0' + i / 10);
+			putchar('0' + i % 10);
+
+			putchar(' ');
+
+			putchar('0' + j / 10);
+			putchar('0' + j % 10);
+
+			if (i == 98 && j == 99)
+				break;
+			putchar(',');
+			putchar(' ');
 		}
 	}
 	putchar('\n');
 	return (0);
 }
-
